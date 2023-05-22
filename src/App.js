6ed48dd3eef207e873fec7c96414b999b437f443
Michael LeMay <mjlemay@gmail.com>
2023-05-21 17:58:15 -0700

    tweaks adlibs section

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react'
+import React, { useState, useEffect, useRef } from 'react';
 import quiz from './quiz';
 
 const ASPECT_RATIO = 0.75; // ~3:4
@@ -24,6 +24,18 @@ const defaultCharacter = {
   persuasion: 0,
   religion: 0,
   survival: 0,
+  bodyWeight: 'unknown',
+  hairColor: 'unknown',
+  hairLength: 'average',
+  eyes: 'unknown',
+  skin: 'unknown',
+  shade: 'average',
+  gender: 'unknown',
+  bodyHeight: 'several feet',
+  grooming: 'average',
+  accessory: 'a blank expression',
+  outerCovering: 'coat',
+  extraLooks: ' ',
 }
 
 // array keys to keep things flat
@@ -47,8 +59,31 @@ function App() {
   const [selectCount, setSelectCount] = useState(0);
   const [selectArr, setSelectArr] = useState([]);
   const [character, setCharacter] = useState(defaultCharacter);
+  const [baseCharacter, setBaseCharacter] = useState({});
+  const [hasCloned, setHasCloned] = useState(false);
   const appRef = useRef(null);
   const { height } = dimentions;
+
+  const { 
+    bodyWeight,
+    hairColor,
+    hairLength,
+    eyes,
+    skin,
+    shade,
+    gender,
+    bodyHeight,
+    grooming,
+    accessory,
+    outerCovering,
+    extraLooks
+  } = character;
+
+  const descTemplate = 
+    `A ${bodyWeight} ${gender} individual of ${shade} ${skin} complexion.
+    The ${accessory} covering them does not mask the depth of thier ${shade} ${eyes} eyes nor
+    obscure thier ${hairLength} ${shade} ${hairColor} hair. Standing ${bodyHeight} tall,
+    their ${grooming} style is evident with a ${outerCovering} cloaking their frame. ${extraLooks}`;
 
   const styles ={
     height,
@@ -62,7 +97,7 @@ function App() {
   }
 
   const  buildCharacter = function (obj) {
-    const updatedCharacter = character;
+    const updatedCharacter = JSON.parse(JSON.stringify(character));
     for (var prop in obj) {
       if (obj.hasOwnProperty(prop)) {
         if (obj[prop] === '+') {
@@ -83,7 +118,7 @@ function App() {
     setSelectCount(0)
     setSelectArr([]);
     if (selected >= quiz.pages.length) {
-      setSection('results');
+      setSection('edit');
     }
   }
   const selectChoice = index => {
@@ -103,6 +138,28 @@ function App() {
     const newHeight = appRef.current.clientWidth * ASPECT_RATIO;
     setDimentions({ width: newWidth, height: newHeight });
   }, []);
+
+  useEffect(() => {
+    if (section === 'edit' && !hasCloned) {
+      const clone = JSON.parse(JSON.stringify(character));
+      console.log('CLOMNGIN G')
+      setBaseCharacter(clone);
+      setHasCloned(true)
+    }
+  }, [character, hasCloned, section]);
+
+  const changeHandler = event => {
+    const modValue = event.target.value;
+    const modName = event.target.name;
+    const mod = JSON.parse(JSON.stringify(character));
+    mod[modName] = modValue;
+    setCharacter({...mod});
+  }
+
+  const focusHandler = event => {
+    console.log('baseCharacter', baseCharacter);
+    setCharacter(baseCharacter);
+  }
 
   return (
     <div className="app" style={styles} ref={appRef}>
@@ -137,14 +194,128 @@ function App() {
           </div>
         </div>
       )}
+      {section === 'edit' &&  (
+        <div>
+          <div className="prompt"><p>Inside is nothing but a small table and a mirror. As you approach the table you see a pile of gear and weapons. You recognize them, because they are yours. You put on your things and Inspect the mirror. You are surprised to see yourself--or rather who you have become through this journey.</p></div>
+          <div className="question"><p>But something's not right. Change one thing about your character's appearance.</p></div>
+          <div className="select"><p>MODIFY ONE</p></div>
+          <div className="prompt">
+            <p>Before you, a&nbsp;
+              <input
+                name="bodyWeight"
+                value={bodyWeight}
+                style={{width: `${bodyWeight.length + 1}ch`}}
+                onChange={changeHandler}
+                onFocus={focusHandler}
+              />&nbsp;
+                <input
+                name="gender"
+                value={gender}
+                style={{width: `${gender.length + 1}ch`}}
+                onChange={changeHandler}
+                onFocus={focusHandler}
+              /> individual of&nbsp;
+              <input
+                name="shade"
+                value={shade}
+                style={{width: `${shade.length + 1}ch`}}
+                onChange={changeHandler}
+                onFocus={focusHandler}
+              />&nbsp;
+              <input
+                name="skin"
+                value={skin}
+                style={{width: `${skin.length + 1}ch`}}
+                onChange={changeHandler}
+                onFocus={focusHandler}
+              /> complexion.
+              The&nbsp;
+              <input
+                name="accessory"
+                value={accessory}
+                style={{width: `${accessory.length + 1}ch`}}
+                onChange={changeHandler}
+                onFocus={focusHandler}
+              /> covering them does not mask the depth of thier&nbsp;
+              <input
+                name="shade"
+                value={shade}
+                style={{width: `${shade.length + 1}ch`}}
+                onChange={changeHandler}
+                onFocus={focusHandler}
+              />&nbsp;
+              <input
+                  name="eyes"
+                  value={eyes}
+                  style={{width: `${eyes.length + 1}ch`}}
+                  onChange={changeHandler}
+                  onFocus={focusHandler}
+                />&nbsp;
+                eyes nor obscure thier&nbsp;
+                <input
+                name="hairLength"
+                value={hairLength}
+                style={{width: `${hairLength.length + 1}ch`}}
+                onChange={changeHandler}
+                onFocus={focusHandler}
+              />&nbsp;
+              <input
+                name="shade"
+                value={shade}
+                style={{width: `${shade.length + 1}ch`}}
+                onChange={changeHandler}
+                onFocus={focusHandler}
+              />&nbsp;
+              <input
+                name="hairColor"
+                value={hairColor}
+                style={{width: `${hairColor.length + 1}ch`}}
+                onChange={changeHandler}
+                onFocus={focusHandler}
+              /> hair. Standing&nbsp;
+              <input
+                name="bodyHeight"
+                value={bodyHeight}
+                style={{width: `${bodyHeight.length + 1}ch`}}
+                onChange={changeHandler}
+                onFocus={focusHandler}
+              />  tall, their&nbsp;
+              <input
+              name="grooming"
+              value={grooming}
+              style={{width: `${grooming.length + 1}ch`}}
+              onChange={changeHandler}
+              onFocus={focusHandler}
+            /> style is evident with a&nbsp;
+            <input
+                name="outerCovering"
+                value={outerCovering}
+                style={{width: `${outerCovering.length + 1}ch`}}
+                onChange={changeHandler}
+                onFocus={focusHandler}
+              /> cloaking their frame.&nbsp;
+            <input
+                name="extraLooks"
+                value={extraLooks}
+                style={{width: `${extraLooks.length + 1}ch`}}
+                onChange={changeHandler}
+                onFocus={focusHandler}
+              /></p>
+          </div>
+          <div className="choices">
+          <button className="choice" onClick={()=> setSection('results')}>CONTINUE</button>
+          </div>
+        </div>
+      )}  
       {section === 'results' &&  (
         <div>
           <div style={{maxHeight: height - 50, overflow: 'auto'}}>
             <p>
-            {Object.entries(character).map(entry => {
-              return(<span>{entry}, </span>)
-            })}
+            {JSON.stringify(character)}
             </p>
+            <div className="prompt">
+              <p>{descTemplate}</p>
+            </div>
           </div>
         </div>
       )}
--- src/quiz.js
+++ src/quiz.js
@@ -95,7 +95,7 @@ const quiz = {
         ]
       },
       {
-        prompt: `Even though you’ve been through so much strife along the way, And you're so tired. You have become stronger with every step of your journey--surviving things no traveler should survive.`,
+        prompt: `Even though you've been through so much strife along the way, And you're so tired. You have become stronger with every step of your journey--surviving things no traveler should survive.`,
         question: `What has become strongest?`,
         limit: 1,
         choices: [
@@ -126,32 +126,317 @@ const quiz = {
         ]
       },
       {
-        prompt: `But throughout everything it’s not just your body that has grown. Your mind--maybe even your soul--has come so far in your journey with you.`,
-        question: `Which of your abilities has grown the most?`,
+        prompt: `No matter how strong you’ve gotten, your memory has faded. Even these memories you can't be sure if they're correct. Now as you carry onward you don’t even know what your seeking. But it draws you forward. Ever forward. Your close now. Closer than you’ve ever been. But your too tired to carry on rightnow--you need to rest. Up ahead you find a field to rest in`,
+        question: `As you set your things down, and lay down yourself you take in the size of this field--how big is the field?`,
         limit: 1,
         choices: [
           {
-            text:'My ability to make choices.',
+            text:'A small field.',
             mods: [
-              {east:'+', arcana: '+'},
+              {bodyWeight:'thin'},
             ]
           },
           {
-            text:'My ability to understand.',
+            text:'A field No bigger or smaller than most.',
             mods: [
-              {north:'+',  religion: '+'},
+              {bodyWeight:'regular sized'},
             ]
           },
           {
-            text:'My ability to trust your gut.',
+            text:'A large field, but not overwhelming.',
             mods: [
-              {west:'+', history: '+'},
+              {west:'heavyset'},
+            ]
+          }
+        ]
+      },
+      {
+        prompt: `Leaning your bodyWeight to one side you see the flora of the field softly waving through your fingertips.`,
+        question: `What grows in this field?`,
+        limit: 1,
+        choices: [
+          {
+            text:'Wild oats.',
+            mods: [
+              {hairColor:'blone'},
             ]
           },
           {
-            text:'My ability to see things as they really are.',
+            text:'Wheat.',
             mods: [
-              {south:'+', investigation: '+'},
+              {hairColor:'brown'},
+            ]
+          },
+          {
+            text:'Barley.',
+            mods: [
+              {hairColor:'red'},
+            ]
+          },
+          {
+            text:'Dead grass.',
+            mods: [
+              {hairColor:'black'},
+            ]
+          }
+        ]
+      },
+      {
+        prompt: `As you layback you realize that it may not be the only thing growing here. Small wildflowers peak up.`,
+        question: `What do they look like?`,
+        limit: 1,
+        choices: [
+          {
+            text:`They are too small, they haven't bloomed yet.`,
+            mods: [
+              {eyes:'green'},
+            ]
+          },
+          {
+            text:'Small flowers white petals with a small blue tint towards the middle.',
+            mods: [
+              {eyes:'blue'},
+            ]
+          },
+          {
+            text:'Small yellow flowers bright.',
+            mods: [
+              {eyes:'amber'},
+            ]
+          },
+          {
+            text:`The flowers are dying they're color faded.`,
+            mods: [
+              {eyes:'brown'},
+            ]
+          },
+          {
+            text:`They are too old. They've wilted and turned dry.`,
+            mods: [
+              {eyes:'gray'},
+            ]
+          }
+        ]
+      },
+      {
+        prompt: `Maybe it's Fate, but it appears to be your favorite season too.`,
+        question: `What season is it?`,
+        limit: 1,
+        choices: [
+          {
+            text:'Spring.',
+            mods: [
+              {skin:'pale'},
+            ]
+          },
+          {
+            text:'Summer.',
+            mods: [
+              {skin:'fair'},
+            ]
+          },
+          {
+            text:'Fall.',
+            mods: [
+              {skin:'tan'},
+            ]
+          },
+          {
+            text:'Winter.',
+            mods: [
+              {skin:'brown'},
+            ]
+          }
+        ]
+      },
+      {
+        prompt: `Rolling your head back towards the sky you breath a relaxed Sigh.`,
+        question: `What time of day is it?`,
+        limit: 1,
+        choices: [
+          {
+            text:'Not sure, Dawn or dusk maybe? Hard to tell if the sun is rising or falling.',
+            mods: [
+              {},
+            ]
+          },
+          {
+            text:`It's day, The sun shines bright and warm on my face.`,
+            mods: [
+              {shade:'light'},
+            ]
+          },
+          {
+            text:`It's late at night the moon hangs, big and beautiful in the sky.`,
+            mods: [
+              {shade:'dark'},
+            ]
+          }
+        ]
+      },
+      {
+        prompt: `In this place you close your eyes, drifting blissfully off to sleep. You're dreams are strange and twisted that night. You dream of a Large clock tower with a blank face. Its hands move in opposite directions and Tick at different speeds. Whatever it is that you seek you know that the answers will be found there.
+        Your awoken by the wind. `,
+        question: `What kind of wind?`,
+        limit: 1,
+        choices: [
+          {
+            text:`A soft breeze that tickles your face and gently brings you back to the waking world.`,
+            mods: [
+              {gender: 'male'},
+            ]
+          },
+          {
+            text:`A harsh breeze that chills you and startles you awake.`,
+            mods: [
+              {gender:'female'},
+            ]
+          },
+          {
+            text:`Your awoken by the wind because you realize there isn't any. Your mind sensing the oddiety stirs you awake.`,
+            mods: [
+              {gender:'non-binary'},
+            ]
+          }
+        ]
+      },
+      {
+        prompt: `Now awakened you see your things beside you where you left them. Undisturbed you look around, Things seem the same.`,
+        question: `What do you do?`,
+        limit: 1,
+        choices: [
+          {
+            text:`Get up and carry on--you've wasted to much time already.`,
+            mods: [
+              {bodyHeight: 'four and a half feet'},
+            ]
+          },
+          {
+            text:`Stay and rest a while longer, things will be bad ahead. Better to rest now while you still can.`,
+            mods: [
+              {bodyHeight:'five and a half feet'},
+            ]
+          },
+          {
+            text:`Not hurried you take your time to get your things together but set out all the same.`,
+            mods: [
+              {bodyHeight:'six and a half feet'},
+            ]
+          }
+        ]
+      },
+      {
+        prompt: `You leave the field. Heading onward. In the distance you see the silhouette of a large clock tower. 
+        The road to this mysterious clock tower was much longer than you expected. Making your way through the forest`,
+        question: `What is the trip like?`,
+        limit: 1,
+        choices: [
+          {
+            text:`A neat but ominous path.`,
+            mods: [
+              {grooming: 'clean'},
+            ]
+          },
+          {
+            text:`A tangled forest.`,
+            mods: [
+              {grooming:'unkept'},
+            ]
+          },
+          {
+            text:`A snarly of undergrowth vines and creatures.`,
+            mods: [
+              {grooming:'disheveled'},
+            ]
+          }
+        ]
+      },
+      {
+        prompt: `You glance at the sky and you realize that it may not even be the same day.`,
+        question: `Just how long has it been?`,
+        limit: 1,
+        choices: [
+          {
+            text:`A few hours if it had been a day, Nothing compared to what I’ve been through before.`,
+            mods: [
+              {hairLength: 'shaved'},
+            ]
+          },
+          {
+            text:`A day at most, I don’t usually lose track of time.`,
+            mods: [
+              {hairLength:'short'},
+            ]
+          },
+          {
+            text:`Maybe a couple of days? It doesn't matter, I'll carry on no matter the time.`,
+            mods: [
+              {hairLength:'medium'},
+            ]
+          },
+          {
+            text:`I’ve lost track at this point, it's so long all the days just blend together. `,
+            mods: [
+              {hairLength:'long'},
+            ]
+          }
+        ]
+      },
+      {
+        prompt: `You make your way onward as your mind considers the time. Then it's finally before you, this clock tower--and within are all the answers you've been seeking.
+        The sun silhouettes it even now obscuring it from your vision.`,
+        question: `What do you do?`,
+        limit: 1,
+        choices: [
+          {
+            text:`Cover my eyes with my hand and get a good look at the tower before continuing onward. `,
+            mods: [
+              {accessory: 'hat'},
+            ]
+          },
+          {
+            text:`A day at most, I don't usually lose track of time.`,
+            mods: [
+              {accessory:'spectacles'},
+            ]
+          },
+          {
+            text:`Maybe a couple of days? It doesn't matter, I'll carry on no matter the time.`,
+            mods: [
+              {accessory:'jewelry'},
+            ]
+          }
+        ]
+      },
+      {
+        prompt: `As you enter into the clock tower you see a spiraling staircase stretching towards the top. 
+        You struggle to think of how tall the tower might truly be. 
+        `,
+        question: `How Tall is it?`,
+        limit: 1,
+        choices: [
+          {
+            text:`The first floor doesn't look too far away.`,
+            mods: [
+              {outerCovering: 'lack of coat'},
+            ]
+          },
+          {
+            text:`It might be a bit of a climb but I’ll make it.`,
+            mods: [
+              {outerCovering:'waist-length coat'},
+            ]
+          },
+          {
+            text:`It's a very long way. I didn't think it would be easy though.`,
+            mods: [
+              {outerCovering:'knee-length overcoat'},
+            ]
+          },
+          {
+            text:`It seems impossibly tall... Might as well start now.`,
+            mods: [
+              {outerCovering:'an ankle-length overcoat'},
             ]
           }
         ]
